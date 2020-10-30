2cbf5496e9c369df15bef9a89c0ee652f5dbcae2
Emily Moore <emilymoore@users.noreply.github.com>
2020-10-29 21:21:05 -0500

    Added relevant Icon Alt text from API

--- script.js
+++ script.js
@@ -63,6 +63,7 @@ function showWeather(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function search(event) {
